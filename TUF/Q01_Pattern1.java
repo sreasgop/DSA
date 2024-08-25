58340d1f76a8c5b3e1d3e521652dde6b747fbafc
sreasgop <sreasgop@gmail.com>
2024-08-25 18:54:39 +0530

    .\Q01_Pattern1.java modified: The program now prints an interactive square based on user input

--- TUF/Q01_Pattern1.java
+++ TUF/Q01_Pattern1.java
@@ -1,14 +1,27 @@
-// 
+// *****
+// *****
+// *****
+// *****
+// *****
+
+import java.util.Scanner;
 
 public class Q01_Pattern1 {
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
-		int n = 5;
+		System.out.print("Input n: ");
+		int n = sc.nextInt();
+		System.out.println();
+		sc.close();
+
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+
 	}
 }
