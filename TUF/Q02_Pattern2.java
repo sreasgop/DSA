8f07aeedcbb366daa390a14662fccbe8242306df
sreasgop <sreasgop@gmail.com>
2024-08-25 18:56:31 +0530

    .\Q02_Pattern2.java modified: Program made interactive

--- TUF/Q02_Pattern2.java
+++ TUF/Q02_Pattern2.java
@@ -4,10 +4,18 @@
 // ****
 // *****
 
+import java.util.Scanner;
+
 public class Q02_Pattern2 {
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
-		int n = 5; // Rows of Pattern
+		System.out.print("Input n: ");
+		int n = sc.nextInt();	// Rows of Pattern
+		System.out.println();
+		sc.close();
+		
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j <= i; j++) {
