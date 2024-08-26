5e65be353aa386bffdc6f32a8852675983827beb
sreasgop <sreasgop@gmail.com>
2024-08-26 05:59:18 +0530

    Q08_Pattern8.java added

---
+++ TUF/Q08_Pattern8.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Q08_Pattern8 {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Input n: ");
+        int row = sc.nextInt();
+        
+        sc.close();
+
+        for (int i = 0; i < row; i++) {
+
+            // Inner-Loop for spaces 
+            for (int j = 0; j < i ; j++) {
+                System.out.print(" ");
+            }
+            
+            // Inner-loop for 1st Up-side-down-triangle of stars
+            for (int k = row; k > i; k--) {
+                System.out.print("*");
+            }
+
+            // Inner-loop for 2nd Up-side-down-triangle of stars
+            for(int l = row - 1; l > i; l--){
+                System.out.print("*");
+            }
+
+            System.out.println();
+ 
+        }
+    }
+}
