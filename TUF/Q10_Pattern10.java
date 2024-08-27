674845a7e9b81b5ad7111e08ef6696e3bcc7b47a
sreasgop <sreasgop@gmail.com>
2024-08-27 16:35:31 +0530

    An improved algorithm has been used to generate the pattern which gets the job done using one nested for loop with the help of an external variable.

--- TUF/Q10_Pattern10.java
+++ TUF/Q10_Pattern10.java
@@ -18,28 +18,46 @@ public class Q10_Pattern10 {
         int n = sc.nextInt();
         sc.close();
 
-        arrow(n);
+        improvedPatern10(n);
 
     }
 
-    public static void arrow(int row){
+    // public static void arrow(int row){
         
-        // UPWARD Triangle:
-        for(int i = 0;  i < row; i++){
-            for(int j = 0; j <= i; j++){
+    //     // UPWARD Triangle:
+    //     for(int i = 0;  i < row; i++){
+    //         for(int j = 0; j <= i; j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+
+    //     // DOWNWARD Triangle:
+    //     for(int i = 0; i < row-1; i++){
+    //         for(int j = row-1; j > i; j--){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+
+    // }
+
+    static void improvedPatern10(int row){
+        
+        for(int i = 1; i <= (2*row-1); i++) 
+        {
+            // Stars: 
+            int stars = i; 
+            if(i>row) {
+                stars = row - (i - row);
+            }
+
+            for(int j=0; j<stars; j++){
                 System.out.print("*");
             }
-            System.out.println();
-        }
+            System.out.println();   
 
-        // DOWNWARD Triangle:
-        for(int i = 0; i < row-1; i++){
-            for(int j = row-1; j > i; j--){
-                System.out.print("*");
-            }
-            System.out.println();
         }
-
     }
 
 }
