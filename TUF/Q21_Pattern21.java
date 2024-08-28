c156618fb27765c352d2b64feb53fc2b2da6ce3d
sreasgop <sreasgop@gmail.com>
2024-08-28 08:15:34 +0530

    .\Q21_Pattern21.java added

---
+++ TUF/Q21_Pattern21.java
@@ -0,0 +1,34 @@
+// * * * * *
+// *       *
+// *       *
+// *       *
+// * * * * *
+
+import java.util.Scanner;
+
+public class Q24_Pattern21 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input n: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        square(n);
+        
+    }
+
+    static void square(int row){
+        for(int i=0; i<row; i++){
+            for(int j=0; j<row; j++){
+                if(i == 0 || i == row-1 || j == 0 || j == row -1 ){
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
