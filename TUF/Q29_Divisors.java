1e80e9b33a4b55e9f47ab165c210be755ce3fcbc
sreasgop <sreasgop@gmail.com>
2024-08-29 08:29:21 +0530

    Q29_Divisors.java modified: New improved function added to the program which runs for square root of N, which is more efficient.

--- TUF/Q29_Divisors.java
+++ TUF/Q29_Divisors.java
@@ -1,24 +1,49 @@
 // Given an integer N, return all divisors of N.
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Q29_Divisors {
-   public static void main(String[] args) {
-        
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Input n: ");
         int n = sc.nextInt();
         sc.close();
 
-        printDivisors(n);
-   }
+        printDivisorsImproved(n);
+    }
 
-   static void printDivisors(int num){
-        System.out.printf("Divisors of %d are: ", num);
-        for(int i=1; i<=num; i++){
-            if(num%i==0){
-                System.out.print(i+" ");
+    // static void printDivisors(int num) {
+    // System.out.printf("Divisors of %d are: ", num);
+    // for (int i = 1; i <= num; i++) {
+    // if (num % i == 0) {
+    // System.out.print(i + " ");
+    // }
+    // }
+    // }
+
+    static void printDivisorsImproved(int num) {
+
+        List<Integer> listObj = new ArrayList<>();
+
+        int limit = (int) Math.sqrt(num);
+        for (int i = 1; i < limit; i++) {
+            if (num % i == 0) {
+                listObj.add(i);
+                if (num / i != i) {
+                    listObj.add(num / i);
+                }
             }
         }
-   }
+
+        Collections.sort(listObj);
+
+        for (int i : listObj) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
