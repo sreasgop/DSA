92603c36a54470f96cc57a0e11616bdb6adc6cff
sreasgop <sreasgop@gmail.com>
2024-08-31 16:28:06 +0530

    Q42 renamed

--- TUF/Q42_IntegerHashing.java
+++ TUF/Q42_IntegerHashing.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Q42_DivisionRuleHasing {
+public class Q42_IntegerHashing {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
