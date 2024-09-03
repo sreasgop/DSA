d5224ae3445e39814cea17be1f211ef85073e194
sreasgop <sreasgop@gmail.com>
2024-09-03 13:04:37 +0530

    Q43_CharacterHashing.java modified: Support for all case characters added

--- TUF/Q43_CharacterHashing.java
+++ TUF/Q43_CharacterHashing.java
@@ -13,7 +13,7 @@ public class Q43_CharacterHashing {
 		int[] hasharray = new int[26];
 
 		for (int i = 0; i < str.length(); i++) {
-			hasharray[str.charAt(i) - 'a']++;
+			hasharray[str.toLowerCase().charAt(i) - 'a']++;
 		}
 
 		System.out.print("Number of characters to check for frequency: ");
@@ -29,7 +29,7 @@ public class Q43_CharacterHashing {
 		}
 
 		while (n-- != 0) {
-			char ch = sc.next().charAt(0);
+			char ch = sc.next().toLowerCase().charAt(0);
 			System.out.print(hasharray[ch - 'a']);
 		}
 
