5bdfef7edac58ab57a76bb8d61f0aa3c06aa621e
sreasgop <sreasgop@gmail.com>
2024-09-07 22:53:57 +0530

    Q46_CountFrequency.java added

---
+++ TUF/Q46_CountFrequency.java
@@ -0,0 +1,9 @@
+// Given an array, we have found the number of occurrences of each element in the array
+
+import java.util.Scanner;
+
+public class Q46_CountFrequency {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
